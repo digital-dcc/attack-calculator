825495d23ad2d6f5387ed68a3edd857e2e912fbc
Richard Walker <digitalsadhu@gmail.com>
2024-07-13 22:50:12 +1200

    add more weapons

--- data/weapons.js
+++ data/weapons.js
@@ -1,5 +1,13 @@
 export const weapons = new Map([
   [
+    'Unarmed',
+    {
+      damage: '1d3',
+      melee: true,
+      subdualDamage: true,
+    },
+  ],
+  [
     'Battleaxe',
     {
       damage: '1d10',
@@ -15,6 +23,38 @@ export const weapons = new Map([
       sneakDamage: '2d6',
       cost: 3,
       subdualDamage: true,
+      melee: true,
+    },
+  ],
+  [
+    'Hammer',
+    {
+      damage: '1d4',
+      cost: 3,
+      melee: true,
+    },
+  ],
+  [
+    'Crowbar',
+    {
+      damage: '1d4',
+      cost: 3,
+      melee: true,
+    },
+  ],
+  [
+    'Pick',
+    {
+      damage: '1d4',
+      cost: 3,
+      melee: true,
+    },
+  ],
+  [
+    'Stick',
+    {
+      damage: '1d4',
+      cost: 3,
       melee: true,
     },
   ],
@@ -96,7 +136,40 @@ export const weapons = new Map([
     },
   ],
   [
+    'Pitchfork',
+    {
+      damage: '1d8',
+      cost: 3,
+      doubleDamageMountedCharging: true,
+      melee: true,
+    },
+  ],
+  [
     'Staff',
+    {
+      damage: '1d4',
+      cost: 0.5,
+      melee: true,
+    },
+  ],
+  [
+    'Cudgel',
+    {
+      damage: '1d4',
+      cost: 0.5,
+      melee: true,
+    },
+  ],
+  [
+    'Shovel',
+    {
+      damage: '1d4',
+      cost: 0.5,
+      melee: true,
+    },
+  ],
+  [
+    'Pole',
     {
       damage: '1d4',
       cost: 0.5,
@@ -156,6 +229,84 @@ export const weapons = new Map([
     },
   ],
   [
+    'Razor',
+    {
+      range: {short: 10, medium: 20, long: 30},
+      damage: '1d4',
+      sneakDamage: '1d10',
+      thrown: true,
+      cost: 3,
+      addStrengthToDamageAtShortRange: true,
+      melee: true,
+      missile: true,
+    },
+  ],
+  [
+    'Awl',
+    {
+      range: {short: 10, medium: 20, long: 30},
+      damage: '1d4',
+      sneakDamage: '1d10',
+      thrown: true,
+      cost: 3,
+      addStrengthToDamageAtShortRange: true,
+      melee: true,
+      missile: true,
+    },
+  ],
+  [
+    'Knife',
+    {
+      range: {short: 10, medium: 20, long: 30},
+      damage: '1d4',
+      sneakDamage: '1d10',
+      thrown: true,
+      cost: 3,
+      addStrengthToDamageAtShortRange: true,
+      melee: true,
+      missile: true,
+    },
+  ],
+  [
+    'Chisel',
+    {
+      range: {short: 10, medium: 20, long: 30},
+      damage: '1d4',
+      sneakDamage: '1d10',
+      thrown: true,
+      cost: 3,
+      addStrengthToDamageAtShortRange: true,
+      melee: true,
+      missile: true,
+    },
+  ],
+  [
+    'Scissors',
+    {
+      range: {short: 10, medium: 20, long: 30},
+      damage: '1d4',
+      sneakDamage: '1d10',
+      thrown: true,
+      cost: 3,
+      addStrengthToDamageAtShortRange: true,
+      melee: true,
+      missile: true,
+    },
+  ],
+  [
+    'Trowel',
+    {
+      range: {short: 10, medium: 20, long: 30},
+      damage: '1d4',
+      sneakDamage: '1d10',
+      thrown: true,
+      cost: 3,
+      addStrengthToDamageAtShortRange: true,
+      melee: true,
+      missile: true,
+    },
+  ],
+  [
     'Dart',
     {
       range: {short: 20, medium: 40, long: 60},
@@ -168,7 +319,31 @@ export const weapons = new Map([
     },
   ],
   [
+    'Quill',
+    {
+      range: {short: 20, medium: 40, long: 60},
+      damage: '1d3',
+      thrown: true,
+      cost: 0.5,
+      addStrengthToDamageAtShortRange: true,
+      melee: false,
+      missile: true,
+    },
+  ],
+  [
     'Handaxe',
+    {
+      range: {short: 10, medium: 20, long: 30},
+      damage: '1d3',
+      thrown: true,
+      cost: 4,
+      addStrengthToDamageAtShortRange: true,
+      melee: true,
+      missile: true,
+    },
+  ],
+  [
+    'Cleaver',
     {
       range: {short: 10, medium: 20, long: 30},
       damage: '1d3',
